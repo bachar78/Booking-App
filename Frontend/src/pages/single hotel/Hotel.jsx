5476c151035e2445e78fe7c8c6112368b7c1a2fb
Bachar Daowd <bachar.daowd@gmail.com>
2022-05-18 21:12:28 +0200

    Single Hotel page

--- Frontend/src/pages/single hotel/Hotel.jsx
+++ Frontend/src/pages/single hotel/Hotel.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
-
+import './hotel.css'
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import photos from '../../assets/photos'
+import Footer from '../../components/footer/Footer'
+import MailList from '../../components/mailList/MailList'
 const Hotel = () => {
   return (
-    <div>Hotel</div>
+    <div className=''>
+      <Navbar />
+      <Header type='list' />
+      <div className='hotelContainer'>
+        <div className='hotelWrapper'>
+          <button className="bookNow">Book Now!</button>
+          <h1 className='hotelTitle'>Grand Hotel</h1>
+          <div className='hotelAddress'>
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New york</span>
+          </div>
+          <span className='hotelDistance'>
+            Excellent location - 500 from center
+          </span>
+          <span className='hotelPriceHighlight'>
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+          <div className='hotelImages'>
+            {photos.map((image) => (
+              <div className='hotelImgWrapper'>
+                <img src={image.src} alt='' />
+              </div>
+            ))}
+          </div>
+          <div className='hotelDetails'>
+            <div className='hotelDetailsTexts'>
+              <h1 className='hotelTitle'>Stay in the heart of Krakow</h1>
+              <p className='hotelDesc'>
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
+                Street Apartments has accommodations with air conditioning and
+                free WiFi. The units come with hardwood floors and feature a
+                fully equipped kitchenette with a microwave, a flat-screen TV,
+                and a private bathroom with shower and a hairdryer. A fridge is
+                also offered, as well as an electric tea pot and a coffee
+                machine. Popular points of interest near the apartment include
+                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
+                airport is John Paul II International Kraków–Balice, 16.1 km
+                from Tower Street Apartments, and the property offers a paid
+                airport shuttle service.
+              </p>
+            </div>
+            <div className='hotelDetailsPrice'>
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <MailList/>
+      <Footer/>
+    </div>
   )
 }
 
